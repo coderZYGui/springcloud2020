6b4ac466ed5346d2c4ae08b2c0ad5f88af2e26c8
zygui_g <zygui@163.com>
2020-04-27 19:48:43 +0800

    初始化父项目

---
+++ cloud2020/cloud-provider-payment-8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ *
+ * @author zygui
+ * @date Created on 2020/4/27 19:21
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
