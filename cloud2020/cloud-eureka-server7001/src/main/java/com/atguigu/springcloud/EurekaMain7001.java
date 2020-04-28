5492b62be4ed334ba52ffa0fe96055f1e1091d42
zygui_g <zygui@163.com>
2020-04-28 10:14:29 +0800

    16集-EurekaServer服务端安装

---
+++ cloud2020/cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Description:
+ *
+ * @author zygui
+ * @date 2020/4/28 10:04
+ */
+@SpringBootApplication
+@EnableEurekaServer // 是Eureka的服务注册中心
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
