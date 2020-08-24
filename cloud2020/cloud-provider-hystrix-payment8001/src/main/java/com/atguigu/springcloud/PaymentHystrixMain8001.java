2560065c88c35406a84f583a2679d63f0af1ce17
zygui_g <zygui@163.com>
2020-08-24 21:14:38 +0800

    47-21: hystrix--使用JMeter高并发压测出现卡顿

---
+++ cloud2020/cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ *
+ * @author zygui
+ * @date 2020/8/24 20:15
+ */
+@SpringBootApplication
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+}
