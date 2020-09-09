0c9ad761bc9d3bc19df4cdaf7a9d968cce5b1ea1
zygui_g <zygui@163.com>
2020-09-09 16:35:23 +0800

    Sentinel 流控、降级、热点key的测试

--- cloud2020/cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloud2020/cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,6 +1,10 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/9/7 15:20
  */
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
-    public String testA() {
+    public String testA() throws InterruptedException {
+        // TimeUnit.MILLISECONDS.sleep(6000); // 用来测试Sentinel的线程数阈值
         return "----testA";
     }
 
@@ -21,5 +27,39 @@ public class FlowLimitController {
     public String testB() {
         return "----testB";
     }
+
+    @GetMapping("/testD")
+    public String testD() {
+
+        // 测试 RT 熔断降级
+        /*try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("testD 测试RT");*/
+
+        // 测试 异常比例 熔断降级
+        /*log.info("testD 测试异常比例");
+        int age = 10 / 0;*/
+
+        // 测试 异常数 熔断降级
+        log.info("testD 测试异常数");
+        int age = 10 / 0;
+        return "----testD";
+    }
+
+    @GetMapping("/testC")
+    @SentinelResource(value = "testC", blockHandler = "deal_testC") //此处value值，一般为@GetMapping属性值去掉斜杠
+    public String testC(@RequestParam(value = "id", required = false) Integer id,
+                        @RequestParam(value = "name", required = false) String name) {
+        return "-----testC";
+    }
+    // 兜底方法
+    public String deal_testC(Integer id, String name, BlockException exception) {
+        // sentinel的默认提示都是： Blocked by Sentinel (flow limiting)
+        return "----deal_testHotKey, o(╥﹏╥)o";
+    }
+
 }
 
